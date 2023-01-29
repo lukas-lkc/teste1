6bb73d68cf7ca58ac3bd5f722266cff98c5eaa32
Lucas Carvalho <32030579+lukas-lkc@users.noreply.github.com>
2023-01-29 17:08:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const cells = document.querySelectorAll(".board div");
+let currentPlayer = "X";
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", function() {
+    if (this.textContent === "") {
+      this.textContent = currentPlayer;
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
+  });
+}
 
